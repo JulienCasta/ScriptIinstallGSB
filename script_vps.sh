70eb92ed451b8d6b77340c7114877d42452c1967
Julien CASTA <juliencasta16@gmail.com>
2019-12-09 11:16:58 +0100

    Initial commit

---
+++ script_vps.sh
@@ -0,0 +1,15 @@
+15926&sedrft
+apt-get update
+apt-get dist-upgrade-y
+apt-get install apache2 -y
+apt-get install php -y
+apt-get install mariadb-server -y
+mysql -u root -e "
+use mysql;
+update user set plugin='' where User='root';
+flush privileges;"
+systemctl restart mariadb.service
+apt-get install php-mysql -y
+chown -R www-data:www-data /var/www
+sed -i 's/.*AllowOverride None.*/AllowOverride All/' /etc/php/*/apache2/php.ini
+systemctl reload apache2
